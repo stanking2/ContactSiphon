df576e05a79e10d09ea1dbd3b5da5ac6f6bd947a
Stan King <stanking2@gmail.com>
2018-07-31 11:05:59 -0500

    pycharm cleanup

--- imapstuff.py
+++ imapstuff.py
@@ -3,31 +3,33 @@
 # out of an email account, checking each email for addresses and names.
 # With credit to Menno Smits (imapclient), Alain Spineux (pyzmail), and
 # Al Sweigart (chps. 14 & 16 of 'Automate the Boring Stuff With Python')
-import csv, sys
+import csv
+import sys
 import imaplib
-import imapclient, pyzmail
+import imapclient
+import pyzmail
 imaplib._MAXLINE = 10000000
 
-## Step 1: Establish a connection to an email server and account
-mailHost = 'imap.mail.yahoo.com' #to be replaced later with user input
+# Step 1: Establish a connection to an email server and account
+mailHost = 'imap.mail.yahoo.com'  # to be replaced later with user input
 imapObj = imapclient.IMAPClient(mailHost, ssl=True)
 userName = input('User Name:')
 passWord = input('Password:')
-if(imapObj.login(userName, passWord))!=b'LOGIN completed':
+if(imapObj.login(userName, passWord)) != b'LOGIN completed':
     print('Connection failed. Exiting program.')
     sys.exit()
 
-## Step 8: Wrap Steps 2-4 to iterate through all folders in the IMAP account
+# Step 8: Wrap Steps 2-4 to iterate through all folders in the IMAP account
 
-## Step 2: Pull the emails in a folder
+# Step 2: Pull the emails in a folder
 imapObj.select_folder('Test', readonly=True)
 UIDs = imapObj.search(['ALL'])
 
-## Step 3: Create the CSV file to hold the data
-outputFile = open('output.csv', 'w', newline='') #add code to select filename and location
+# Step 3: Create the CSV file to hold the data
+outputFile = open('output.csv', 'w', newline='')  # add code to select filename and location
 outputWriter = csv.writer(outputFile)
 
-## Step 4: Iterate through the emails and pull out the addresses
+# Step 4: Iterate through the emails and pull out the addresses
 rawMessages = imapObj.fetch(UIDs, ['BODY[]'])
 for i in UIDs:
     imapObj.fetch(i, 'BODY[]')
@@ -38,10 +40,10 @@ for i in UIDs:
     cBc = message.get_addresses('bcc')
     outputWriter.writerow([cFr, cTo, cCc, cBc])
 
-## Step 5: Validate the addresses
+# Step 5: Validate the addresses
 
-## Step 6: Eliminate the duplicate entries
+# Step 6: Eliminate the duplicate entries
 
-## Ste[ 7: Close the CSV and the connection]
+# Ste[ 7: Close the CSV and the connection]
 outputFile.close()
 imapObj.logout()
