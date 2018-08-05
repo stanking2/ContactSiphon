596fe669fdcfeed98ac0ad12e77603027505da86
Stan King <stanking2@gmail.com>
2018-08-05 01:00:10 -0500

    successful run of imapstuff module, pull of 1569 msgs, UnicodeEncodeError trapped.
    Signed-off-by: Stan King <stanking2@gmail.com>

--- imapstuff.py
+++ imapstuff.py
@@ -1,68 +1,72 @@
 #! python3
-# This is my first attempt at an app to pull contact info
-# out of an email account, checking each email for addresses and names.
-# With credit to Menno Smits (imapclient), Alain Spineux (pyzmail), and
-# Al Sweigart (chps. 14 & 16 of 'Automate the Boring Stuff With Python')
+""" This is my first attempt at an app to pull contact info
+    out of an email account, checking each email for addresses and names.
+    With credit to Menno Smits (imapclient), Alain Spineux (pyzmail), and
+    Al Sweigart (chps. 14 & 16 of 'Automate the Boring Stuff With Python')"""
 import csv
-import sys
 import imaplib
 import imapclient
+from imapclient.util import to_unicode
 import pyzmail
 imaplib._MAXLINE = 10000000
 
-# Step 1: Establish a connection to an email server and account
-mailHost = 'imap.mail.yahoo.com'  # to be replaced later with user input
+# Establish a connection to an email server and account
+mailHost = 'imap.mail.att.net'  # to be replaced later with GUI input
 imapObj = imapclient.IMAPClient(mailHost, ssl=True)
-userName = input('User Name:')
+userName = input('Email Address:')
 passWord = input('Password:')
-if(imapObj.login(userName, passWord)) != b'LOGIN completed':
-    print('Connection failed. Exiting program.')  # add retry code
-    sys.exit()
 
-# Step 2: Create the CSV file to hold the data
-outputFile = open('output.csv', 'w', newline='')  # add code to select filename and location
+# convert to try/catch for NO or BAD response
+print(imapObj.login(userName, passWord))
+# if imapObj.login(userName, passWord) != b'LOGIN completed':
+#     print('Connection failed. Exiting program.')
+#     sys.exit()  # b'[AUTHENTICATIONFAILED] LOGIN Invalid credentials'
+# else:
+#     print('Successful connection!')
+
+# Create the CSV file to hold the siphoned contacts
+outputFile = open('output-' + userName + '.csv', 'w', newline='')  # add code to select filename and location
 outputWriter = csv.writer(outputFile)
-adrs = 0
+outputWriter.writerow(['FROM', 'TO', 'CC', 'BCC'])
 
 # Step 3: Wrap Steps 4-5 to iterate through all folders in the IMAP account
 folders = imapObj.list_folders()
 k = 0
-outputWriter.writerow(['FROM', 'TO', 'CC', 'BCC'])
+adrs = 0
 while k < len(folders):
     # print(folders[k][2])
     # Step 4: Pull the emails in a folder
     imapObj.select_folder(folders[k][2], readonly=True)
-    UIDs = imapObj.search(['ALL'])
-    print('Now processing ' + folders[k][2] + '...')
+    UIDs = imapObj.search(['ALL'])  # Can I chg to ENVELOPE HERE AS WELL?
+    print('Now processing ' + folders[k][2] + '...')  # Add count of msgs
 
     # Step 5: Iterate through the emails and pull out the addresses
-    rawMessages = imapObj.fetch(UIDs, ['BODY[]'])
+    rawMessages = imapObj.fetch(UIDs, ['BODY[]'])  # Chg to ENVELOPE
+    if len(UIDs) == 1:
+        msgtxt = ' message.'
+    else:
+        msgtxt = ' messages.'
+    print(folders[k][2] + ' contains ' + str(len(UIDs)) + msgtxt)
     for i in UIDs:
         imapObj.fetch(i, 'BODY[]')
         message = pyzmail.PyzMessage.factory(rawMessages[i][b'BODY[]'])
-        cFr = message.get_addresses('from')
+        # Chg to ENVELOPE
+        cFr = list(message.get_address('from'))
+        cFr[0] = to_unicode(cFr[0])
         cTo = message.get_addresses('to')
         cCc = message.get_addresses('cc')
         cBc = message.get_addresses('bcc')
-        # trap UnicodeEncodeError
-        outputWriter.writerow([cFr, cTo, cCc, cBc])
+        try:
+            outputWriter.writerow([cFr, to_unicode(cTo), to_unicode(cCc), to_unicode(cBc)])
+        except UnicodeEncodeError as err:
+            outputWriter.writerow(['', to_unicode(cTo), to_unicode(cCc), to_unicode(cBc)])
         adrs = adrs + 1
     print(folders[k][2] + ' completed.')
     k = k + 1
 
-# Step 6: Eliminate the duplicate entries
-    # sort the csv
-    # concatenate addresses
-    # compare one row to the next, if == then delete second row
-# Step 6B: Pare the entries
-    # remove brackets []
-    # start new line at '), ('
-    # split addresses into name and email address cells
-    # remove all wrappers: ()[]''
-    # sort and remove duplicates again
-
 # Step 5: Validate the addresses
 
+# Step 6: Eliminate the duplicate entries
 print('Done! ' + str(adrs) + ' emails pulled for addresses.')
 
 # Step 7: Close the CSV and the connection]
